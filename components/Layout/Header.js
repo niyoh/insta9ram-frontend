1e0e0d5528bfd00d5507d14e1bd6de6dd4737fb2
Martin <martinhy59@hotmail.com>
2016-10-12 05:26:40 +0800

    styling

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -27,7 +27,6 @@ class Header extends React.Component {
       <header className={`mdl-layout__header ${s.header}`} ref={node => (this.root = node)}>
         <div className={`mdl-layout__header-row ${s.row}`}>
           <Link className={`mdl-layout-title ${s.title}`} to="/">
-            insta9ram
           </Link>
           <div className="mdl-layout-spacer"></div>
         </div>
